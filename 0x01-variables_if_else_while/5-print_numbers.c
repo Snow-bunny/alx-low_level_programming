5c5205980657765f27782eda340da95d3e6f2568
snow-bunny <agolden21@yahoo.com>
2022-12-09 17:42:29 -0800

    love

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print num btw 0 t0 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
