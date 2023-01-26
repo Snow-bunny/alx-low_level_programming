c029c09538f99a509d9e2a353254ccca12234bc6
snow-bunny <agolden21@yahoo.com>
2023-01-26 12:52:36 -0800

    ghost

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,34 @@
+; File: 101-hello_holberton.asm
+
+; Auth: EBILASS EL-ISRAEL
+
+; Desc: 64-bit assembly program that prints
+
+;       Hello, Holberton followed by a new line.
+
+
+extern printf
+
+section .text
+
+	global main
+
+main:
+
+	push rbp
+
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+
+	mov rax,0
+	ret
+
+
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
