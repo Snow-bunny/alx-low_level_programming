24fd095a5b85c6f0fcb06abdf807e7b11cb1410b
snow-bunny <agolden21@yahoo.com>
2023-01-31 05:34:24 -0800

    grace

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (node = 0; node < (index - 1); node++)
 	{
 		if (copy->next == NULL)
-			returm (-1);
+			return (-1);
 		copy = copy->next;
 	}
 	tmp = copy->next;
