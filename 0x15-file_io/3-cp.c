dd41176d39879b1f9dcdf25f0780c52ec41a6b74
snow-bunny <agolden21@yahoo.com>
2023-02-06 13:32:33 -0800

    race

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,6 +14,7 @@ void close_file(int fd);
 char *create_buffer(char *file)
 {
 	char *buffer;
+
 	buffer = malloc(sizeof(char) * 1024);
 
 	if (buffer == NULL)
@@ -33,6 +34,7 @@ char *create_buffer(char *file)
 void close_file(int fd)
 {
 	int c;
+
 	c = close(fd);
 
 	if (c == -1)
